1fb2c4e78f4966d8d8106d0e84fe127654ee62ba
SashaMois <56739595+SashaMois@users.noreply.github.com>
2020-03-01 17:41:31 +0300

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Interest_accrul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sum = 0; /* current summary */
+            decimal constribution = 0;
+
+            Console.WriteLine("Hello, User! Today you can receive additional deposit ammount.\n" 
+                               + "What contribution do you want to make?");
+            do
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+
+                    if ((input == "quit") || (input == "Quit") || (input == "QUIT")
+                        || (input == "q") || (input == "Q"))
+                    {
+                        Console.WriteLine("Good Bye!");
+                        return;
+                    }
+
+                    constribution += checked(Convert.ToDecimal(input));
+                    break;
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("The inputed object is not decimal.\nPlease, try again.");
+                }
+            } while (true);
+
+            decimal deposit = 0;
+            if (constribution < 100)
+                deposit = 5;
+            else if ((constribution >= 100) || (constribution < 200))
+                deposit = 7;
+            else if (constribution >= 200)
+                deposit = 10;
+            sum = constribution += constribution / 100 * deposit;
+
+            Console.WriteLine($"The operation was succesfully! Your current balance - {sum}.\nGood Bye!");
+            Console.ReadKey();
+        }
+    }
+}
